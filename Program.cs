c5d33ec909e439b99fe62a2e9a70be95c2d0f128
Honkole <144032620+Honkole@users.noreply.github.com>
2023-09-25 16:28:42 +0300

    11111

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* задание 1
+            Console.WriteLine("Введите число x");
+            int x = Convert.ToInt32(Console.ReadLine());
+            if (x == 0) Console.WriteLine("Ты зачем 0 ввёл? Ваш ответ 6.");
+            else
+            {
+               int ops = (7 * (x * x) + 3 * x) + 6;
+                Console.WriteLine($"Ваше число {ops}");
+            }
+            */
+            /* задание 1.2
+            var abc ="aaaa";
+            var abc1 = Convert.ToInt32(abc);
+            Console.Write("Введите х пжпжжпжпж: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a == 0) Console.WriteLine("Ты зачем нулём накакал? Я всё вижу ( ваше значение 12 )");
+            else
+            {
+                if (a == abc1) Console.WriteLine("Оче сильно накакал, не осилил буквы"); 
+                int y = (12 * a * a + 7 * a + 12);
+                Console.WriteLine($"Ваше число {y}");
+            }
+            */
+            /* задание 2
+            Console.Write("Введите х: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int p = 4 * x;
+            Console.WriteLine($"Ваш периметр равен: {p}");
+            */
+            /* задание 3
+            Console.Write("Ведите радиус: ");
+            int radiys = Convert.ToInt32(Console.ReadLine());
+            int diametr = radiys * 2;
+            Console.WriteLine($"Ваш диметр: {diametr}");
+            */
+
+        }
+
+    }
+}
